428d45c268b38956c2cdfa52b99421029330a868
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-05-10 23:11:39 +0300

    Extract Attractions component
    Took 8 minutes

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,27 +1,7 @@
-import { useEffect, useState } from 'react';
-import { Attraction } from '../models/attraction.ts';
-import axios from 'axios';
+import Attractions from '../../features/attractions/Attractions.tsx';
 
 function App() {
-  const [attractions, setAttractions] = useState<Attraction[]>([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:7000/api/attractions').then((response) => {
-      console.log(response);
-      setAttractions(response.data);
-    });
-  }, []);
-
-  return (
-    <>
-      <h1>Attractions</h1>
-      <ol>
-        {attractions.map((attraction) => (
-          <li key={attraction.id}>{attraction.name}</li>
-        ))}
-      </ol>
-    </>
-  );
+  return <Attractions></Attractions>;
 }
 
 export default App;
