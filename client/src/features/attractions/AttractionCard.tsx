85ab16a733a1efeed038e03a4279e315e47a1f6f
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-05-18 19:39:37 +0300

    Stylize the AttractionCard
    Took 1 hour 20 minutes

--- client/src/features/attractions/AttractionCard.tsx
+++ client/src/features/attractions/AttractionCard.tsx
@@ -1,20 +1,67 @@
 import { Attraction } from '../../app/models/attraction.ts';
 
+const imageSize = '45em';
+
+const attractionProperty = {
+  margin: 0,
+  marginBottom: '0.5rem',
+  display: 'flex',
+  alignItems: 'center',
+};
+
+const attractionPropertyTitle = {
+  ...attractionProperty,
+  fontWeight: 'bold',
+  marginTop: '0.5rem',
+};
+
+const descriptionStyle = {
+  backgroundColor: '#A89D9D',
+  width: '100%',
+  margin: '0.25em',
+  paddingLeft: '0.5em',
+  paddingTop: '0.25em',
+};
+
 interface Props {
   attraction: Attraction;
 }
 
 export default function AttractionCard({ attraction }: Props) {
+  const leftSideProperties = {
+    city: attraction.cityId,
+    address: attraction.address,
+    website: <a href={attraction.website}>{attraction.website}</a>,
+  };
+
   return (
     <div style={{ backgroundColor: 'lightgrey' }}>
       <img
         src={attraction.mainPictureUrl}
         alt={attraction.name}
-        style={{ height: '40em', width: '40em' }}
+        style={{ height: imageSize, width: imageSize }}
       />
-      <div>
-        <p style={{ margin: 0 }}>{attraction.name}</p>
-        <p style={{ margin: 0 }}>{attraction.address}</p>
+      <div style={{ display: 'flex', margin: '1em' }}>
+        <div>
+          <h3 title="name" style={attractionPropertyTitle}>
+            {attraction.name}
+          </h3>
+
+          {Object.entries(leftSideProperties).map(([name, value]) => (
+            <p title={name} style={attractionProperty}>
+              <img
+                src={`/icons/${name}.svg`}
+                alt={name}
+                style={{ marginRight: '0.5rem' }}
+              ></img>
+              {value}
+            </p>
+          ))}
+        </div>
+
+        <div title="description" style={descriptionStyle}>
+          {attraction.description}
+        </div>
       </div>
     </div>
   );
