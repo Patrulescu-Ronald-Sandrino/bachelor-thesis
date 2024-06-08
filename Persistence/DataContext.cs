5617f817698f83f019e9bfe6d9e28bcd95f7dfe3
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-06-08 23:36:58 +0300

    add Guid.OfChar
    Took 17 minutes

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,7 +11,7 @@ public class DataContext(DbContextOptions options) : IdentityDbContext<User, Use
     public DbSet<AttractionType> AttractionTypes { get; init; }
     public DbSet<Attraction> Attractions { get; init; }
     public DbSet<Country> Countries { get; init; }
-    public DbSet<Reaction> Reactions { get; set; }
+    public DbSet<Reaction> Reactions { get; init; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -20,8 +20,7 @@ public class DataContext(DbContextOptions options) : IdentityDbContext<User, Use
         var roleTypes = EnumUtils.GetValues<UserRoles>().ToArray();
         var userRoles = Enumerable.Range(0, roleTypes.Length).Select(i => new UserRole
             {
-                Id = Guid.Parse(Guid.Empty.ToString()
-                    .Replace(Guid.Empty.ToString().First().ToString(), ((int)roleTypes[i]).ToString())),
+                Id = Guid.Empty.OfChar((char)(roleTypes[i] + '0')),
                 Name = roleTypes[i].ToString(),
                 NormalizedName = roleTypes[i].ToString().ToUpper()
             })
--- Utils/TypesExtensions.cs
+++ Utils/TypesExtensions.cs
@@ -1,6 +1,14 @@
+using System.Text.RegularExpressions;
+
 namespace Utils;
 
-public static class TypesExtensions
+public static partial class TypesExtensions
 {
     public static string Repeat(this string s, int n) => string.Concat(Enumerable.Repeat(s, n));
+
+    public static Guid OfChar(this Guid guid, char c) =>
+        Guid.Parse(NotDashRegex().Replace(guid.ToString(), c.ToString()));
+
+    [GeneratedRegex("[^-]")]
+    private static partial Regex NotDashRegex();
 }
