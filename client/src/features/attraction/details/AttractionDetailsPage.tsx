e1f281d15ae74a17a938571ae045fb1ecd8f2055
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-07-14 18:40:48 +0300

    Remove old implementation of AttractionDetailsPage
    Took 10 minutes

--- client/src/features/attraction/details/AttractionDetailsPage.tsx
+++ client/src/features/attraction/details/AttractionDetailsPage.tsx
@@ -2,25 +2,22 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import agent from '../../../app/api/agent.ts';
 import { Attraction } from '../../../app/models/attraction.ts';
-import AttractionCardContainer from './AttractionCardContainer.tsx';
+import Loadable from '../../../app/layout/Loadable.tsx';
+import NotFound from '../../../app/errors/NotFound.tsx';
 
 export default function AttractionDetailsPage() {
   const { id } = useParams<{ id: string }>();
   const [attraction, setAttraction] = useState<Attraction | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     agent.Attractions.fetch(id!)
       .then((response) => setAttraction(response))
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
   }, [id]);
 
-  return (
-    <>
-      {attraction ? (
-        <AttractionCardContainer attraction={attraction} />
-      ) : (
-        <div>Not found....</div>
-      )}
-    </>
-  );
+  if (!loading && !attraction) return <NotFound />;
+
+  return <Loadable loading={loading}>{attraction!.name}</Loadable>;
 }
