7067a5d2ea40e5560ca696313ee561568b720c7f
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-08-22 20:54:36 +0300

    Create a React production build
    Took 3 minutes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../API/wwwroot',
+  },
   server: {
     host: '0.0.0.0',
     port: 4000,
