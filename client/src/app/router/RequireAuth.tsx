1ce92912d4467f86fcbabadaad55ccfb9524b4cc
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-08-22 15:14:19 +0300

    Defer route Outlet rendering in RequireAuth to after the verification

--- client/src/app/router/RequireAuth.tsx
+++ client/src/app/router/RequireAuth.tsx
@@ -1,8 +1,9 @@
 import { useAppSelector } from '../store/configureStore.ts';
 import { Outlet, useLocation } from 'react-router-dom';
-import { router } from './Routes.tsx';
-import { useEffect } from 'react';
 import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
+import { router } from './Routes.tsx';
+import Loadable from '../layout/Loadable.tsx';
 
 interface Props {
   roles?: string[];
@@ -12,14 +13,21 @@ export default function RequireAuth({ roles }: Props) {
   const { user } = useAppSelector((state) => state.account);
   const location = useLocation();
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    setLoading(false);
     if (!user) {
       void router.navigate('/login', { state: { from: location } });
     } else if (roles && !roles.some((r) => user.roles?.includes(r))) {
       toast.error('Not authorized to access this area');
       void router.navigate('/attractions');
     }
-  }, [location, roles, user]);
+  }, [user, roles, location]);
 
-  return <Outlet />;
+  return (
+    <Loadable loading={loading} target="app">
+      <Outlet />
+    </Loadable>
+  );
 }
